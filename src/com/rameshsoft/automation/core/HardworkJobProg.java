31be80ccce2f864da0524df9940815803e3e48e1
rameshsoft9177791456 <rameshsoft.9177791456@gmail.com>
2024-03-19 07:18:32 +0530

    dev2 is added new file

---
+++ src/com/rameshsoft/automation/core/HardworkJobProg.java
@@ -0,0 +1,12 @@
+package com.rameshsoft.automation.core;
+
+public class HardworkJobProg {
+
+	public void hardwork() {
+		System.out.println("Hardwork");
+	}
+	
+	public void job() {
+		System.out.println("JOB");
+	}
+}
